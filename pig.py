bb70cc62d62267fac46405529c8c02efc9c9632b
Caitlin Swickard <caitlin6029@gmail.com>
2024-07-10 16:17:19 -0600

    start of players turn

--- pig.py
+++ pig.py
@@ -23,19 +23,18 @@ while True:
     players = int(players)
     # check num between 2-4
     if 2 <= players <= 4:
-      print("Must be between 2-4 players.")
       break
+    else:
+      print("Must be between 2-4 players.")
+
   else:
     # invalid num try again
-    print("invalid number, try again.")
-print(players)
+    print("Invalid number, try again.")
+# print(players)
 
 
-
-
-# variables
-  # max_score
-  # player_scores - list of all the scores for players - list comp for unknown size of list
+max_score = 50
+player_scores = [0 for _ in range(players)]
 
 # go through players turns
 # while players scores are less than max-score
@@ -43,6 +42,18 @@ print(players)
   # grab that roll - if that roll = 1 - turn is done
   # else: add the value to players score
 # for loop around this while - to get each players index
+while max(player_scores) < max_score:
+  # check if player wants to roll
+  should_roll = input("Do you want to roll? Press y: ")
+  if should_roll.lower() != 'y':
+    break
+  value = roll()
+
+  # check if value was a 1
+  if value == 1:
+    print("You rolled a 1, turn DONE!")
+  else:
+    print("You rolled a:", value)
 
 # max_score exit once all the turn have completed rolls
 # exit - with who won
